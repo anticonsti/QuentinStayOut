2f4e75848032afb9e1a06991c72ff2fce47762a9
flo <florent_lay@hotmail.fr>
2014-04-20 10:17:20 +0200

    publierannonce dans la bonne classe et readInt et readString dans Utils

--- Navigation.java
+++ Navigation.java
@@ -7,94 +7,12 @@ public class Navigation {
     static Scanner in = new Scanner(System.in);
     static ConnectionAirChambre connecte;
 
-    
-    public static void publierAnnonce(){
-
-	System.out.println("Pour pouvoir publier une annonce, il faut que vous soyez connecte.");
-    	System.out.println("Veuillez entrer votre choix :");
-	System.out.println("-------------------------------------------------------------");
-	System.out.println("0 - retour");
-	System.out.println("1 - S'inscrire");
-	System.out.println("2 - Se connecter");
-	System.out.println("-------------------------------------------------------------");
-	int choix = readInt();
-	try{
-	    switch(choix){
-	    case 0 :
-		System.out.print("\033c"); //nettoyage de l'ecran
-		System.out.println("Veuillez entrer votre choix :");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("0 - fin");
-		System.out.println("1 - Publier votre annonce");
-		System.out.println("2 - Trouvez un logement");
-		System.out.println("-------------------------------------------------------------");
-		break;
-	    case 1 : 
-		System.out.println("Nom entre 1~20caracteres.");
-		String nom = readString();
-		if(!nom.matches("[a-z]{1,20}")) System.out.println("Syntaxe incorrecte");
-	   
-		System.out.println("Prenom entre 1~30caracteres");
-		String prenom = readString();
-		if(!nom.matches("[a-z]{1,30}"))  System.out.println("Syntaxe incorrecte");
-
-		System.out.println("Pseudo entre 1~30caracteres");
-		String pseudo = readString();
-		if(!nom.matches("[a-z]{1,30}"))  System.out.println("Syntaxe incorrecte");
-	   
-		System.out.println("Mot de entre 1~30caracteres");
-		String mdp = readString();
-		if(!nom.matches("[a-z]{1,30}"))  System.out.println("Syntaxe incorrecte");
-
-		connecte.inscription(4654684, nom, prenom, pseudo, mdp);
-		//NOMBRE AU PIF POUR COMPILER CETTE MERDE
-
-		
-		//  si un client supprime son compte, son id_proprietaire ne sera pas repris par une autre personne :
-		//  de toute facon on n'aura jamais de INT nombre de clients sur le site. 
-		
-		break;
-	    case 2 :
-		System.out.print("Pseudo : ");
-		String id = readString();
-		if(!id.matches("[a-z]{1,30}")) System.out.println("Syntaxe incorrecte");
-	   
-		System.out.print("\nPassword : ");
-		String pw = readString();
-		if(!pw.matches("[a-z0-9]{1,30}"))  System.out.println("Syntaxe incorrecte");
-
-	
-		connecte.connection(id,pw);
-		
-		
-		break;
-	    default :
-		System.out.println("Pour pouvoir publier une annonce, il faut que vous soyez connecte.");
-		System.out.println("Veuillez entrer votre choix :");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("0 - retour");
-		System.out.println("1 - S'inscrire");
-		System.out.println("2 - Se connecter");
-		System.out.println("-------------------------------------------------------------");
-		break;
-	    
-	    }
-	}catch (SQLException e) {
-
-	    System.err.println(e.getMessage());
-	}
-
-    }
-    
 
     /** Imprime le menu a l'ecran.*/
     public static int printMenu() {
 	int c = -1; // le choix de l'utilisateur
 	
-	// -------------------
 	// Impression du menu
-	// -------------------
-	
 	System.out.print("\033c"); //nettoyage de l'ecran
 	System.out.println("Veuillez entrer votre choix :");
 	System.out.println("-------------------------------------------------------------");
@@ -103,26 +21,19 @@ public class Navigation {
 	System.out.println("2 - Trouvez un logement");
 	System.out.println("-------------------------------------------------------------");
 	
-	// ----------------------------
 	// Lecture du choix utilisateur
-	// ----------------------------
-	
-	c = readInt();
+	c = Utils.readInt();
 	
 	System.out.print("\033c"); //nettoyage de l'ecran
 	
-	// -------------------------------
 	// traitement du choix utilisateur
-	// -------------------------------
 
 
 	PublierAnnonce pa = new PublierAnnonce(connecte.getConnection());
 	ChercherLogement cl = new ChercherLogement(connecte.getConnection());
 
-	//	try{
 	switch(c){
 	case 1 : 
-	    //publierAnnonce();
 	    pa.printMenuPublierAnnonce();
 	    break;
 
@@ -143,36 +54,10 @@ public class Navigation {
 	    System.out.println("2 - Trouvez un logement");
 	    System.out.println("-------------------------------------------------------------");
 	    break;
-	}/*
-	}catch (SQLException e) {
-	    System.err.println(e.getMessage());
 	}
-	 */
 	return c;
-
-
     }
 
-    static public String readString(){
-	try{
-	    return in.next();
-	}
-	catch(Exception e){
-	    e.printStackTrace();
-	    return null;
-	}
-    }
-    
-    static public int readInt(){
-	try{
-	    return in.nextInt();   //lecture du choix utilisateur    
-	}
-	catch(Exception e){
-	    in.nextLine();
-	    e.printStackTrace();
-	    return -1;
-	}
-    }
     
     public static void print(String s, int i) {
 	System.out.print(s);
@@ -187,28 +72,22 @@ public class Navigation {
 	System.exit(1);
     }
     
+
     /**Cree la connexion a la base et attend les instructions de l'utilisateur.*/
     public static void main(String[] args) {
-	// ---------------------------
+
 	// Verification des parametres
-	// ---------------------------
-	
 	if (args.length != 1)
 	    usage();
-
 	
 	try{
-	    // -------------------
+
 	    // Connexion a la base
-	    // --------------------
 	    String password = PasswordField.readPassword("Entrer votre mot de passe pour vous connecter a Postgres: ");
 	    connecte = new ConnectionAirChambre(args[0], password);
-		
-		
-	    // ---------------------------------------
+
+
 	    // Impression du menu. Pour finir, tapez 0
-	    // ---------------------------------------
-		
 	    int c = -1;
 	    while(c != 0){
 		c = printMenu();
@@ -217,14 +96,10 @@ public class Navigation {
 		    System.in.read();
 		}
 	    }
-		
-		
-	    // -------------------------
+
 	    // fermeture de la connexion
-	    // -------------------------
 	    connecte.close();
-	    in.close();
-		
+	    in.close();	
 	}
 	catch(Exception e)
 	    {
