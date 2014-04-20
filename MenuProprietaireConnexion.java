3cfbe587e4b2e8d07429ec61e50b308b444661da
flo <florent_lay@hotmail.fr>
2014-04-20 22:32:27 +0200

    facultatif avec regex {0,...} et appuyer sur entree

--- MenuProprietaireConnexion.java
+++ MenuProprietaireConnexion.java
@@ -17,12 +17,12 @@ public class MenuProprietaireConnexion{
 	System.out.println("Compte " + pseudo);
     	System.out.println("Veuillez entrer votre choix :");
 	System.out.println("-------------------------------------------------------------");
-	System.out.println("0 - deconnexion");
+	System.out.println("0 - déconnexion");
 	System.out.println("1 - ajouter un logement");
 	System.out.println("2 - visualiser les logements");
 	System.out.println("3 - supprimer un logement");
 	System.out.println("4 - offre promotionnelle");
-	System.out.println("5- modification d'un logement");
+	System.out.println("5 - modification d'un logement");
 	System.out.println("6 - visualiser les locations");
 
 	System.out.println("-------------------------------------------------------------");
@@ -31,60 +31,65 @@ public class MenuProprietaireConnexion{
 	Logement lgm = new Logement(conn);
 
 	try{
-	switch(choix){
-	case 0:
-	    break;
+	    switch(choix){
+	    case 0:
+		break;
 
-	case 1:
-	    System.out.println("Votre logement est un appartement(A) ou une chambre(C)? entrer A ou C.");
-	    String typeLogement = Utils.readString("[0-9a-z]{1,20}");	
+	    case 1:
+		System.out.println("Votre logement est un appartement(A) ou une chambre(C)? entrer A ou C.");
+		String typeLogement = Utils.readString("[0-9a-z]{1,20}");	
 	
-	    System.out.println("Adresse entre 1~20caracteres.");
-	    String adresse = Utils.readString("[0-9a-z]{1,20}");
+		System.out.println("Adresse (1~20 caracteres): ");
+		String adresse = Utils.readString("[0-9a-z]{1,20}");
 
-	    System.out.println("Surface: ");
-	    int surface= Utils.readInt();
+		System.out.println("Surface: ");
+		int surface= Utils.readInt();
 
-	    System.out.println("Ville entre 1~30caracteres");
-	    String ville = Utils.readString("[A-Za-z]{1,20}");
+		System.out.println("Ville entre (1~30 caracteres): ");
+		String ville = Utils.readString("[A-Za-z]{1,20}");
 	   
-	    System.out.println("Date début disponibilité en format YYYY/MM/DD");//format sql annee/mois/jour
-	    String dateDep = Utils.readString("date");
+		System.out.println("Date début disponibilité (format DD/MM/YYYY): ");//format sql annee/mois/jour
+		String dateDep = Utils.readString("date");
 
-	    System.out.println("Date fin disponibilité en format YYYY/MM/DD");
-	    String dateFin = Utils.readString("date");		
+		System.out.println("Date fin disponibilité (format DD/MM/YYYY): ");
+		String dateFin = Utils.readString("date");		
 
-	    System.out.println("Prix du logement par jour.");
-	    int prixJour = Utils.readInt();		
-	    /* IL FAUT VERIFIER QUE l'UTILISATEUR A REPONDU AUX prints FACULTATIF AVEC HASNEXT..() JE PENSE
-	    System.out.println("(facultatif) Prix du logement par mois (cas 27jours ou plus)");
-	    if(hasNextInt()) {int prixMois = Utils.readString();}
+		//System.out.println("Prix du logement par jour.");
+		//int prixJour = Utils.readInt();
+		System.out.println("Prix: ");
+		int prix = Utils.readInt();
 
-	    System.out.println("(facultatif) Suggestions");
-	    if(hasNextLine()){String sugg = Utils.readString("date");}		
+		//IL FAUT VERIFIER QUE l'UTILISATEUR A REPONDU AUX prints FACULTATIF AVEC HASNEXT..() JE PENSE hasNextInt() hasNextLine()
+		// Pour passer au champ suivant il faut appuyer sur Entree
+		// comme c'est facultatif, on met {0,...}
+		System.out.println("(facultatif) Prix du logement par mois (cas 27 jours ou plus): ");
+		String prixMois = Utils.readString("[0-9]{0,5}");
 
-	    System.out.println("(facultatif) Prestations");
-	    if(hasNextLine()){String prest = Utils.readString("date");}		
+		System.out.println("(facultatif) Suggestions: ");
+		String sugg = Utils.readString("[A-Za-z]{0,20}");		
 
-	    System.out.println("(facultatif) Photos");//boucle infini jusqu'a ce que l'utilisateur dit FALSE
-	    if(hasNextLine()){String photo = Utils.readString("date");}	
+		System.out.println("(facultatif) Prestations");
+		String prest = Utils.readString("[A-Za-z]{0,20}");	
 
-	    System.out.println("(facultatif) Transport");//il faut 2print pour nb vehicule,prix transport
-	    if(hasNextLine()){String transport = Utils.readString("[A-Za-z]{1,20}");}	
-	    */
+		System.out.println("(facultatif) Photos");//boucle infini jusqu'a ce que l'utilisateur dit FALSE
+		String photo = Utils.readString("[A-Za-z]{0,20}");
+
+		System.out.println("(facultatif) Transport");//il faut 2print pour nb vehicule,prix transport
+		String transport = Utils.readString("[A-Za-z]{0,20}");
 	    
-	    lgm.ajouterLogementDispo(dateDep,dateFin);
-	    lgm.ajouterLogementLogement(adresse,surface,ville);
-	    lgm.ajouterLogementPrix(prixJour);
+	    
+		lgm.ajouterLogementDispo(dateDep,dateFin);
+		lgm.ajouterLogementLogement(adresse,surface,ville);
+		lgm.ajouterLogementPrix(prix);
 
-	    /*
-	    if(sugg!="") lgm.ajouterLogementSuggestion();
-	    if(prest!="") lgm.ajouterLogementPrestation();
-	    if(photo!="") lgm.ajouterLogementPhoto();
-	    if(transport!="") lgm.ajouterLogementTransport();
-	    */	    
+		/*
+		  if(sugg!="") lgm.ajouterLogementSuggestion();
+		  if(prest!="") lgm.ajouterLogementPrestation();
+		  if(photo!="") lgm.ajouterLogementPhoto();
+		  if(transport!="") lgm.ajouterLogementTransport();
+		*/	    
 
-	    break;
+		break;
 
 	    case 2: 
 		break;
@@ -102,9 +107,9 @@ public class MenuProprietaireConnexion{
 
 		break;
 	    }
-	    }catch (SQLException e) {
+	}catch (SQLException e) {
 	    System.err.println(e.getMessage());
-	    }
+	}
     }
 
 
