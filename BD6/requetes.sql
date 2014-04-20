e296e70b68c00fd258bd6df70b9ea918f5efff4e
flo <florent_lay@hotmail.fr>
2014-04-20 14:39:17 +0200

    modif requetes

--- BD6/requetes.sql
+++ BD6/requetes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE proprietaire(
-id_proprietaire INT PRIMARY KEY NOT NULL,
+id_proprietaire SERIAL PRIMARY KEY,
 nom_proprietaire VARCHAR(20) NOT NULL, 
 prenom_proprietaire VARCHAR(30) NOT NULL, 
 pseudo VARCHAR(30) NOT NULL, 
@@ -8,10 +8,21 @@ mot_de_passe VARCHAR(30) NOT NULL
 
 
 CREATE TABLE logement(
-id_logement INT PRIMARY KEY NOT NULL,
+id_logement SERIAL PRIMARY KEY,
 adresse_logement VARCHAR(100) NOT NULL,
 surface INT NOT NULL CHECK(surface>0), 
 ville VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE appartement(
+id_logement INT REFERENCES logement(id_logement), 
+nb_pieces INT CHECK(nb_pieces>0)
+);
+
+
+CREATE TABLE chambre(
+id_logement INT REFERENCES logement(id_logement)
 );
 
 
@@ -22,14 +33,14 @@ id_logement INT REFERENCES logement(id_logement) ON DELETE CASCADE
 
 
 CREATE TABLE photo(
-id_photo INT PRIMARY KEY NOT NULL,
+id_photo SERIAL PRIMARY KEY,
 id_logement INT REFERENCES logement(id_logement),
 nom_photo VARCHAR(100) DEFAULT ('sans nom')
 );
 
 
 CREATE TABLE offre_promotionnelle(
-id_promo INT PRIMARY KEY, 
+id_promo SERIAL PRIMARY KEY, 
 id_logement INT REFERENCES logement(id_logement),
 date_debut_offre_promo DATE NOT NULL, 
 date_fin_offre_promo DATE NOT NULL, 
@@ -39,7 +50,7 @@ CHECK ( date_fin_offre_promo >= date_debut_offre_promo )
 
 
 CREATE TABLE disponibilite(
-id_dispo INT PRIMARY KEY, 
+id_dispo SERIAL PRIMARY KEY, 
 date_debut_dispo DATE NOT NULL, 
 date_fin_dispo DATE NOT NULL
 CHECK ( date_fin_dispo >= date_debut_dispo )
@@ -47,14 +58,14 @@ CHECK ( date_fin_dispo >= date_debut_dispo )
 
 
 CREATE TABLE suggestion(
-id_suggestion INT PRIMARY KEY, 
+id_suggestion SERIAL PRIMARY KEY, 
 type_suggestion VARCHAR(30), 
 nom_suggestion VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE prestation(
-id_prestation INT PRIMARY KEY, 
+id_prestation SERIAL PRIMARY KEY, 
 description_prestation VARCHAR(100) NOT NULL, 
 prix_prestation INT NOT NULL CHECK(prix_prestation>0)
 );
@@ -74,7 +85,7 @@ id_prestation INT REFERENCES prestation(id_prestation)
 
 
 CREATE TABLE service_transport(
-id_service_transport INT PRIMARY KEY, 
+id_service_transport SERIAL PRIMARY KEY, 
 nb_vehicule INT NOT NULL CHECK(nb_vehicule>=0),
 prix_transport INT NOT NULL CHECK(prix_transport>0)
 );
@@ -87,7 +98,7 @@ id_service_transport INT REFERENCES service_transport(id_service_transport)
 
 
 CREATE TABLE location(
-id_location INT PRIMARY KEY, 
+id_location SERIAL PRIMARY KEY, 
 date_debut_location DATE NOT NULL,
 date_fin_location DATE NOT NULL, 
 montant_total INT NOT NULL CHECK(montant_total>0),
@@ -108,7 +119,7 @@ id_service_transport INT REFERENCES service_transport(id_service_transport)
 
 
 CREATE TABLE locataire(
-id_locataire INT PRIMARY KEY, 
+id_locataire SERIAL PRIMARY KEY, 
 nom_locataire VARCHAR(20) NOT NULL, 
 prenom_locataire VARCHAR(30) NOT NULL, 
 adresse_locataire VARCHAR(50) NOT NULL, 
